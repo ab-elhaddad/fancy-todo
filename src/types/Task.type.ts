1482d4d4189d79465819bf88c4456c53c82c3178
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-08 20:26:23 +0300

    Updated Task type

--- src/types/Task.type.ts
+++ src/types/Task.type.ts
@@ -3,7 +3,7 @@ type Task = {
 	t_title: string;
 	t_description?: string;
 	t_status?: boolean;
-	t_due_date?: Date;
+	t_due_date?: Date | string;
 	t_created_at?: Date;
 	t_user_id: number;
 } & { [key: string]: any };
