1dc84a977ff7f13ebc305823bfd13da88207e99e
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-08 19:18:48 +0300

    Fix return bugs

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -11,27 +11,27 @@ authenticate.use(async (req: Request, res: Response, next: NextFunction) => {
 		const token = String(req.headers.authorization);
 		if (!token)
 			// verifying that there is a token attatched
-			res.status(401).json({ message: 'You have to enter a token!' }); // Unauthorized
+			return res.status(401).json({ message: 'You have to enter a token!' }); // Unauthorized
 		const rawToken = token.split(' ')[1];
 
 		try {
 			// verifying that the token's schema is valid
 			jwt.verify(rawToken, config.jwtSecretKey);
 		} catch (e) {
-			res.status(401).json({ message: 'Invalid Token!' }); // Unauthorized
+			return res.status(401).json({ message: 'Invalid Token!' }); // Unauthorized
 		}
 
 		const user = jwt.decode(rawToken) as JwtPayload;
 
 		if (!(await doesExist(user.id, user.email)))
 			// verifying that there is a user matched with the token
-			res.status(401).json({ message: 'Wrong or old token!' });
+			return res.status(401).json({ message: 'Wrong or old token!' });
 
 		res.locals.user = user;
 		next();
 	} catch (e) {
 		console.error('Error in authenticate middleware\n', e);
-		res.json({ message: 'Internal Server Error.' });
+		return res.json({ message: 'Internal Server Error.' });
 	}
 });
 
