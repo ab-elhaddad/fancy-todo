5b1d57d4b8d2760f059f9dfea9f956f2d7ce6168
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-09 14:22:40 +0300

    Updated test anfig and script

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-// jest.config.js
 module.exports = {
 	testEnvironment: 'node',
 	setupFilesAfterEnv: ['./jest.setup.js'],
 	testMatch: ['**/__tests__/**/*.test.ts'],
 	globals: {
-		'process.env.TEST_DATABASE_URL': 'postgres://user:password@localhost:5432/testdb',
+		'process.env.TEST_DATABASE_URL': 'postgres://user:password@localhost:5432/testdb'
 	},
+	verbose: true,
+	detectOpenHandles: true
 };
