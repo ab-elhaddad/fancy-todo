f7c8652edc030ebb19bdfbb4181a051c2d374dfc
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-11 22:11:26 +0300

    Made a seperate conn to testing db

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -6,5 +6,8 @@ export const config = {
 	saltRounds: Number(process.env.SALT_ROUNDS),
 	jwtSecretKey: String(process.env.JWT_SECRET_KEY),
 	gmailUser: String(process.env.GMAIL_USER),
-	gmailPass: String(process.env.GMAIL_PASS)
+	gmailPass: String(process.env.GMAIL_PASS),
+	env: String(process.env.ENV),
+	db: String(process.env.DATABASE_URL),
+	testDb: String(process.env.TEST_DATABASE_URL),
 };
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import { config } from '../configuration/config';
 
 //const prisma = new PrismaClient({ log: ['query'] });
-const prisma = new PrismaClient();
+const prisma = (config.env === 'dev' ? new PrismaClient() : new PrismaClient({ datasources: { db: { url: config.testDb } } }));
 export default prisma;
