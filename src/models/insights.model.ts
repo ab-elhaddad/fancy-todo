8be5d3e3217718ae2977b5efdb95316d0e73fb94
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-13 19:42:10 +0300

    Made code more readable

--- src/models/insights.model.ts
+++ src/models/insights.model.ts
@@ -8,13 +8,43 @@ class insights {
 	 */
 	static async getTasksInitiatedByMe(u_id: number) {
 		const insights = {
-			allTasks: await prisma.task.count({ where: { t_user_id: u_id } }),
-			completedTasks: await prisma.task.count({ where: { t_user_id: u_id, t_status: true } }),
+			allTasks: await prisma.task.count({
+				where: {
+					t_user_id: u_id
+				}
+			}),
+			completedTasks: await prisma.task.count({
+				where: {
+					t_user_id: u_id,
+					t_status: true
+				}
+			}),
 			incompleteTasks: -1,
-			overdueTasks: await prisma.task.count({ where: { t_user_id: u_id, t_status: false, t_due_date: { lt: new Date() } } }),
-			highPriorityTasks: await prisma.task.count({ where: { t_user_id: u_id, t_priority: 3 } }),
-			mediumPriorityTasks: await prisma.task.count({ where: { t_user_id: u_id, t_priority: 2 } }),
-			lowPriorityTasks: await prisma.task.count({ where: { t_user_id: u_id, t_priority: 1 } }),
+			overdueTasks: await prisma.task.count({
+				where: {
+					t_user_id: u_id,
+					t_status: false,
+					t_due_date: { lt: new Date() }
+				}
+			}),
+			highPriorityTasks: await prisma.task.count({
+				where: {
+					t_user_id: u_id,
+					t_priority: 3
+				}
+			}),
+			mediumPriorityTasks: await prisma.task.count({
+				where: {
+					t_user_id: u_id,
+					t_priority: 2
+				}
+			}),
+			lowPriorityTasks: await prisma.task.count({
+				where: {
+					t_user_id: u_id,
+					t_priority: 1
+				}
+			}),
 			lists: (
 				await prisma.list.findMany({
 					where: { l_user_id: u_id },
