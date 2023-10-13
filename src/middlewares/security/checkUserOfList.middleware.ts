3e39432b33c920d2afe18dbc46631a9ea4e438ce
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-13 15:59:49 +0300

    prettier

--- src/middlewares/security/checkUserOfList.middleware.ts
+++ src/middlewares/security/checkUserOfList.middleware.ts
@@ -7,7 +7,7 @@ import express, { Request, Response, NextFunction, Router } from 'express';
 const checkUserOfList: Router = express.Router();
 checkUserOfList.use(async (req: Request, res: Response, next: NextFunction) => {
 	const { id: userID } = res.locals.user;
-	const listId: (number | undefined) = req.query.l_id || req.body.l_id;
+	const listId: number | undefined = req.query.l_id || req.body.l_id;
 	const found = await prisma.list.findFirst({
 		where: {
 			l_id: listId,
