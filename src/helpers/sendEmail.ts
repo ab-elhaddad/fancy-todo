768350ecb04a4394b4fd0432ad9c36799e3fa84c
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-14 18:53:28 +0300

    Made a seperate class for email services

---
+++ src/helpers/sendEmail.ts
@@ -0,0 +1,41 @@
+import transporter from '../lib/transporter';
+import { config } from '../configuration/config';
+import jwt from 'jsonwebtoken';
+
+class sendEmail {
+	/**
+	 *  Sends an email with a confirmation link to the user to confirm his account.
+	 * @param userID
+	 * @param userEmail
+	 */
+	static async confirmation(userID: number, userEmail: string) {
+		// Genertes a token based only on the id.
+		const token = jwt.sign(String(userID), config.jwtSecretKey);
+		const url = `http://localhost:3000/confirm/${token}`;
+
+		await transporter.sendMail({
+			from: 'Fancy To-Do',
+			to: userEmail,
+			subject: 'Email confirmation',
+			html: `Please click this link to confirm your email: <a href="${url}">${url}</a>`
+		});
+	}
+
+	static async resetPassword(userID: number, userEmail: string) {
+		const payload = {
+			email: userEmail,
+			id: userID
+		};
+		const token = jwt.sign(payload, config.jwtSecretKey, { expiresIn: '1h' });
+		const url = `http://localhost:3000/users/reset-password/${token}`;
+
+		await transporter.sendMail({
+			from: 'Fancy To-Do',
+			to: userEmail,
+			subject: 'Reset password',
+			html: `Please click this link to reset your password: <a href="${url}">${url}</a>`
+		});
+	}
+}
+
+export default sendEmail;
