278b106176864c1c36e08314a2bae354804db1dd
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-14 15:43:10 +0300

    Added jsDoc

--- src/middlewares/requestDebug.middleware.ts
+++ src/middlewares/requestDebug.middleware.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { debug } from '../lib/debug';
 
+/**
+ * Middleware that logs every request to the console.
+ */
 const requestDebug = express.Router();
 
 requestDebug.use((req: Request, res: Response, next: NextFunction) => {
