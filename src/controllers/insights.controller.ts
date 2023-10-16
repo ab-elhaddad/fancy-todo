df98788b906ca5cf55b11e8cb25bc8a15e36cd2c
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-16 23:25:39 +0300

    ⛅ Added the weather feature

--- src/controllers/insights.controller.ts
+++ src/controllers/insights.controller.ts
@@ -1,5 +1,7 @@
 import insights from '../models/insights.model';
 import { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import { config } from '../configuration/config';
 
 export const getTasksInsights = async (req: Request, res: Response, next: NextFunction) => {
 	try {
@@ -8,7 +10,45 @@ export const getTasksInsights = async (req: Request, res: Response, next: NextFu
 		res.json({ message: 'Tasks insights retrieved successfully.', insights: tasksInsights });
 	} catch (err) {
 		res.locals.error = err;
-		console.log(err);
 		next();
 	}
 };
+
+export const getWeather = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { city } = req.query;
+		const { lat, lon } = req.query;
+		let result;
+		if (city) {
+			result = (await axios.get(
+				`https://api.openweathermap.org/data/2.5/weather?
+					units=metric&
+					q=${city}&
+					appid=${config.weatherApiKey}`
+			)).data;
+		}
+		else if (lat && lon) {
+			result = (await axios.get(
+				`https://api.openweathermap.org/data/2.5/weather?
+					units=metric&
+					lat=${lat}&
+					lon=${lon}&
+					appid=${config.weatherApiKey}`
+			)).data;
+		}
+		else
+			throw { status: 400, message: 'Invalid request.' };
+
+		res.json({
+			message: 'Weather retrieved successfully.', weather: {
+				main: result.weather[0].main,
+				description: result.weather[0].description,
+				temp: result.main.temp + ' C',
+				humidity: result.main.humidity,
+			}
+		});
+	} catch (err) {
+		res.locals.error = err;
+		next();
+	}
+}
--- src/routes/api/insightsRouter.ts
+++ src/routes/api/insightsRouter.ts
@@ -1,10 +1,11 @@
 import { Application } from 'express';
 import authenticate from '../../middlewares/authenticate.middleware';
-import { getTasksInsights } from '../../controllers/insights.controller';
+import { getTasksInsights, getWeather } from '../../controllers/insights.controller';
 import errorHandler from '../../middlewares/errorHandler.middleware';
 
 const insightsRouter = (app: Application) => {
 	app.get('/insights/tasks', authenticate, getTasksInsights, errorHandler);
+	app.get('/insights/weather', authenticate, getWeather, errorHandler);
 };
 
 export default insightsRouter;
