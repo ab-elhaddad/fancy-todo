4ea692f5863dc89ffa25ed777a867b898e919b91
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-10-18 17:43:26 +0300

    ⛅ Updated the weather feature to display the 5 upcoming days

--- src/controllers/insights.controller.ts
+++ src/controllers/insights.controller.ts
@@ -22,32 +22,40 @@ export const getWeather = async (req: Request, res: Response, next: NextFunction
 		if (city) {
 			result = (
 				await axios.get(
-					`https://api.openweathermap.org/data/2.5/weather?
+					`https://api.openweathermap.org/data/2.5/forecast?
+					cnt=40&
 					units=metric&
 					q=${city}&
 					appid=${config.weatherApiKey}`
 				)
-			).data;
+			).data.list;
 		} else if (lat && lon) {
 			result = (
 				await axios.get(
-					`https://api.openweathermap.org/data/2.5/weather?
+					`https://api.openweathermap.org/data/2.5/forecast?
+					cnt=40&
 					units=metric&
 					lat=${lat}&
 					lon=${lon}&
 					appid=${config.weatherApiKey}`
 				)
-			).data;
+			).data.list;
 		} else throw { status: 400, message: 'Invalid request.' };
+
+		const weather = [];
+		for (let i = 0; i < result.length; i += 7) {
+			weather.push({
+				main: result[i].weather[0].main,
+				description: result[i].weather[0].description,
+				temp: result[i].main.temp + ' C',
+				humidity: result[i].main.humidity,
+				date: result[i].dt_txt
+			});
+		}
 
 		res.json({
 			message: 'Weather retrieved successfully.',
-			weather: {
-				main: result.weather[0].main,
-				description: result.weather[0].description,
-				temp: result.main.temp + ' C',
-				humidity: result.main.humidity
-			}
+			weather: weather
 		});
 	} catch (err) {
 		res.locals.error = err;
