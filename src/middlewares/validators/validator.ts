f7df483d3ef20d9e92b2c9eee3d2201b08c94640
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-11-02 20:02:16 +0200

    Add subtask validation middleware and router

--- src/middlewares/validators/validator.ts
+++ src/middlewares/validators/validator.ts
@@ -1,9 +1,11 @@
 import tasksValidator from "./tasks.validator";
 import usersValidate from "./users.validator"
+import subtasksValidator from "./subtask.validator";
 
 class Validator {
 	static users = usersValidate;
 	static tasks = tasksValidator;
+	static subtasks = subtasksValidator;
 }
 
 export default Validator;
--- src/routes/api/subtaskRouter.ts
+++ src/routes/api/subtaskRouter.ts
@@ -3,12 +3,31 @@ import authenticate from '../../middlewares/authenticate.middleware';
 import errorHandler from '../../middlewares/errorHandler.middleware';
 import { createSubtask, deleteSubtask, revStatus, updateSubtask } from '../../controllers/subtasks.controller';
 import checkUserOfTask from '../../middlewares/security/checkUserOfTask.middleware';
+import Validator from '../../middlewares/validators/validator';
 
 const subtaskRouter = (app: Application) => {
-	app.post('/subtasks', authenticate, createSubtask, errorHandler);
-	app.delete('/subtasks', authenticate, checkUserOfTask, deleteSubtask, errorHandler);
-	app.put('/subtasks', authenticate, updateSubtask, errorHandler);
-	app.put('/subtasks/rev-status', authenticate, checkUserOfTask, revStatus, errorHandler);
+	app.post('/subtasks',
+		authenticate,
+		Validator.subtasks.create,
+		createSubtask,
+		errorHandler);
+	app.delete('/subtasks',
+		authenticate,
+		checkUserOfTask,
+		Validator.subtasks.delete,
+		deleteSubtask,
+		errorHandler);
+	app.put('/subtasks',
+		authenticate,
+		Validator.subtasks.update,
+		updateSubtask,
+		errorHandler);
+	app.put('/subtasks/rev-status',
+		authenticate,
+		checkUserOfTask,
+		Validator.subtasks.revStatus,
+		revStatus,
+		errorHandler);
 };
 
 export default subtaskRouter;
