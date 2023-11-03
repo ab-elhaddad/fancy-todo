1f12f5a80c15242c31921dcc4c5ec2a1943c031d
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-11-03 05:29:20 +0200

    Added JSDoc

--- src/helpers/setDates.ts
+++ src/helpers/setDates.ts
@@ -1,5 +1,10 @@
 import Task from '../types/Task.type';
 
+/**
+ * Set the time part of date properties in Task objects to a specific hour (12).
+ * *this is to avoid issues with timezones.*
+ * @param task Task to set dates on
+ */
 function setDates(task: Task) {
 	if (task.t_due_date) task.t_due_date = new Date(new Date(task.t_due_date).setHours(12));
 	if (task.t_recurring?.end_date) task.t_recurring.end_date = new Date(new Date(task.t_recurring.end_date).setHours(12));
