daa5b035282fae07629ef5da95c108d99e285803
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-11-07 10:23:06 +0200

    Added t_priorty validation

--- src/helpers/setTaskPriority.ts
+++ src/helpers/setTaskPriority.ts
@@ -3,11 +3,13 @@ import Task from './../types/Task.type';
  * Sets the priority of the task from string to a number.
  */
 const setTaskPriority = (task: Task) => {
-	if (task.t_priority) {
-		if (task.t_priority === 'low') task.t_priority = 1;
-		else if (task.t_priority === 'medium') task.t_priority = 2;
-		else if (task.t_priority === 'high') task.t_priority = 3;
-	}
+  const err = Error('Invalid priority. Must be "low", "medium", or "high"');
+  if (!task?.t_priority) throw err;
+
+  if (task.t_priority === 'low') task.t_priority = 1;
+  else if (task.t_priority === 'medium') task.t_priority = 2;
+  else if (task.t_priority === 'high') task.t_priority = 3;
+  else throw err;
 };
 
 export default setTaskPriority;
