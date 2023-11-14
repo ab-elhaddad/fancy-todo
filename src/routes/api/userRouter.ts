b68290d68d9266a3ad81d5af4a190c789aa21895
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-11-14 14:37:42 +0200

    Removed auth in sign out

--- src/routes/api/userRouter.ts
+++ src/routes/api/userRouter.ts
@@ -15,36 +15,36 @@ import authenticate from '../../middlewares/authenticate.middleware';
 import Validator from '../../middlewares/validators/validator';
 
 const userRouter = (app: Application) => {
-  app.route('/users/sign-in')
+  app
+    .route('/users/sign-in')
     .get(signIn.get)
     .post(Validator.users.signIn, signIn.post)
     .all(errorHandler);
 
-  app.route('/users/sign-up')
+  app
+    .route('/users/sign-up')
     .get(signUp.get)
     .post(Validator.users.signUp, signUp.post)
     .all(errorHandler);
 
-  app.route('/users/sign-out')
-    .all(authenticate)
-    .get(signOut)
-    .all(errorHandler);
+  app.route('/users/sign-out').get(signOut).all(errorHandler);
 
-  app.route('/users/forgot-password')
+  app
+    .route('/users/forgot-password')
     .get(forgotPassword.get)
     .post(Validator.users.forgotPassword, forgotPassword.post)
     .all(errorHandler);
 
-  app.route('/users/reset-password/:token')
+  app
+    .route('/users/reset-password/:token')
     .get(resetPassword.get)
     .post(Validator.users.resetPassword, resetPassword.post)
     .all(errorHandler);
 
-  app.route('/confirm/:token')
-    .get(Validator.users.confirm, confirm)
-    .all(errorHandler);
+  app.route('/confirm/:token').get(Validator.users.confirm, confirm).all(errorHandler);
 
-  app.route('/users/profile')
+  app
+    .route('/users/profile')
     .all(authenticate)
     .get(profile)
     .put(Validator.users.update, updateUser)
